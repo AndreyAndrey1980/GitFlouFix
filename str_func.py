7202bb43729d4ca1a27091c2107c5a3987172668
AndreyAndrey1980 <mandibul1@mail.ru>
2024-06-10 18:22:05 +0300

    add docstring to second func

--- str_func.py
+++ str_func.py
@@ -7,6 +7,15 @@ def to_uppercase(s: str) -> str:
     """
     return s.upper()
 
+def capitalize_words(s: str) -> str:
+    """
+    Делает заглавными первые буквы каждого слова в строке s и возвращает результат.
+
+    :param s: Строка для преобразования.
+    :return: Строка с первыми буквами каждого слова в верхнем регистре.
+    """
+    return ' '.join(word.capitalize() for word in s.split())
+
 input_string = "привет, мир. как дела?"
 uppercased_string = to_uppercase(input_string)
 print(uppercased_string)
